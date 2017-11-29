61d9ee97ec6acd7716d3f4b9dcdd92631649a095
qi741223802 <741223802@qq.com>
2017-11-29 16:39:03 +0800

    day 3

---
+++ www/test.py
@@ -0,0 +1,12 @@
+import orm
+from models import User,Blog,Comment
+import asyncio
+
+async def test(loop):
+	await orm.create_pool(loop=loop,user='root',password='',db='awesome')
+	u = User(name='Test',email='test@example.com',password='1234567890',image='about:blank')
+	await u.save()
+	await orm.destroy_pool()
+loop = asyncio.get_event_loop()
+loop.run_until_complete(test(loop))
+loop.close()
